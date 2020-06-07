8c52bb8129c889b608d26221cb5a019e13d24d22
Arun <apschauhan60@gmail.com>
2020-06-07 19:28:59 +0530

    initial-commit

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,39 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            author
+            slug
+            images{
+             sharp: childImageSharp{
+                fluid(
+                  maxWidth:100
+                  maxHeight:100
+                  
+                ){
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  `);
+
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    author: post.frontmatter.author,
+    slug: post.frontmatter.slug,
+    images: post.frontmatter.images,
+    excerpt: post.excerpt,
+  }));
+};
+
+export default usePosts;
